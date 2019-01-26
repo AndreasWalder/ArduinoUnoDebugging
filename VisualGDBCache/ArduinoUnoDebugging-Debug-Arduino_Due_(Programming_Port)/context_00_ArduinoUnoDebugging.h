989e662325cd2306645a0b29eac48e8a8de05587
Andreas <andreas.walder@web.de>
2019-01-26 23:53:51 +0100

    Projektdateien hinzufügen.

---
+++ VisualGDBCache/ArduinoUnoDebugging-Debug-Arduino_Due_(Programming_Port)/context_00_ArduinoUnoDebugging.h
@@ -0,0 +1,3 @@
+#pragma once
+#include "C:/Users/Andreas/source/repos/ArduinoUnoDebugging/ArduinoUnoDebugging/VisualGDBCache/ArduinoUnoDebugging-Debug-Arduino_Due_(Programming_Port)/GeneratedPrototypes.h"
+
